e9b658d6e4fb1125e187c80665cef8d95509851f
Ritesh Sharma <rs3907367@gmail.com>
2026-05-01 17:38:50 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,24 +1,4 @@
-// // src/utils/api.js
-// import axios from 'axios'
 
-// const api = axios.create({
-//   baseURL: '/api',
-//   withCredentials: true,
-//   headers: { 'Content-Type': 'application/json' }
-// })
-
-// api.interceptors.response.use(
-//   r => r,
-//   err => {
-//     if (err.response?.status === 401) {
-//       document.cookie = 'pf_token=;expires=Thu,01 Jan 1970 00:00:00 UTC;path=/'
-//       if (!window.location.pathname.includes('/auth')) window.location.href = '/auth'
-//     }
-//     return Promise.reject(err)
-//   }
-// )
-
-// export default api
 
 import axios from 'axios'
 
